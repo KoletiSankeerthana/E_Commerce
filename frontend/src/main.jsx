fcf4be66c69ab946a70003d1f15540cf71a4920e
Koleti Sankeerthana <sankeerthanakoleti@gmail.com>
2026-02-26 11:17:25 +0530

    Fix BrowserRouter missing in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import "./styles/global.css";
+import { BrowserRouter } from "react-router-dom";   // ADD THIS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>     {/* ADD THIS */}
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
