b1acbef045cb25cecff72999ee30198adbdee462
Koleti Sankeerthana <sankeerthanakoleti@gmail.com>
2026-02-26 13:29:26 +0530

    Fix vercel.json routing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",   // ADD THIS LINE (CRITICAL FIX)
-
+  base: "/",
   plugins: [react()],
-
-  server: {
-    host: true,
-    port: 5173,
-    strictPort: true,
-    cors: true,
-    allowedHosts: true,
-    proxy: {
-      '/api': {
-        target: 'https://ecommerce-vwsy.onrender.com',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
-})
+});
